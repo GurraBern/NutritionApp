644795f2377e9a90d54d03722c3d6c2acd8afbf8
Gustav Berndtzen <gustav.berndtzen@gmail.com>
2023-08-12 18:12:19 +0200

    fixed search results

--- NutritionApp/MVVM/Views/MainPage.xaml.cs
+++ NutritionApp/MVVM/Views/MainPage.xaml.cs
@@ -5,10 +5,17 @@ namespace NutritionApp;
 
 public partial class MainPage : ContentPage
 {
+	private MainViewModel vm;
 	public MainPage(MainViewModel vm)
 	{
 		InitializeComponent();
 		BindingContext = vm;
+		this.vm = vm;
 	}
+
+    private void SearchResultSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+		var test = e.SelectedItem;
+    }
 }
 
