8556705503ee31b07121150c420d6fe05f2a59f4
Gustav Berndtzen <gustav.berndtzen@gmail.com>
2023-08-22 17:29:45 +0200

    fixed nutrition element to display with binded values

--- NutritionApp/MVVM/Views/Components/NutritionElement.xaml.cs
+++ NutritionApp/MVVM/Views/Components/NutritionElement.xaml.cs
@@ -2,8 +2,34 @@ namespace NutritionApp.MVVM.Views;
 
 public partial class NutritionElement : ContentView
 {
-	public NutritionElement()
+    public static readonly BindableProperty TextProperty =
+            BindableProperty.Create(nameof(Text), typeof(string), typeof(NutritionElement), default(string));
+
+    public static readonly BindableProperty AmountProperty =
+            BindableProperty.Create(nameof(Amount), typeof(string), typeof(NutritionElement), default(string));
+
+    
+    public string Amount
+    {
+        get => (string)GetValue(AmountProperty);
+        set => SetValue(AmountProperty, value);
+    }
+
+    public string Text
+    {
+        get => (string)GetValue(TextProperty);
+        set => SetValue(TextProperty, value);
+    }
+
+    public NutritionElement()
 	{
-		InitializeComponent();
-	}
+        InitializeComponent();
+    }
+
+    protected override void OnBindingContextChanged()
+    {
+        base.OnBindingContextChanged();
+
+        nutritionNameLabel.Text = Text + ": " + Amount;
+    }
 }
