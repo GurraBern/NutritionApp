1fa9ff71e7ccee896dea2dddc7e5ff1cc252536c
Gustav Berndtzen <gustav.berndtzen@gmail.com>
2023-10-26 20:00:40 +0200

    [Feat] - Added auto login

--- NutritionApp/Services/AuthService/AuthService.cs
+++ NutritionApp/Services/AuthService/AuthService.cs
@@ -7,7 +7,6 @@ public class AuthService : IAuthService
     private readonly FirebaseAuthClient _firebaseAuth;
     private UserCredential _userCredential;
     public User? CurrentUser => _userCredential?.User;
-
     public AuthService(FirebaseAuthClient firebaseAuth)
     {
         this._firebaseAuth = firebaseAuth;
@@ -16,12 +15,25 @@ public class AuthService : IAuthService
     public async Task SignUp(string email, string password)
     {
         _userCredential = await _firebaseAuth.CreateUserWithEmailAndPasswordAsync(email, password);
-
     }
 
     public async Task Login(string email, string password)
     {
         _userCredential = await _firebaseAuth.SignInWithEmailAndPasswordAsync(email, password);
+        if (_userCredential != null)
+        {
+            await SecureStorage.SetAsync("email", email);
+            await SecureStorage.SetAsync("password", password);
+        }
+    }
+
+    public async Task TryAutoLogin()
+    {
+        var email = await SecureStorage.GetAsync("email");
+        var password = await SecureStorage.GetAsync("password");
+
+        if (email != null && password != null)
+            await Login(email, password);
     }
 
     public void SignOut()
