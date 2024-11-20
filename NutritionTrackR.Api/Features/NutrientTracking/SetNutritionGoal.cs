c34374af128687c4f699e49b4267d69e32c9dcf9
Gustav Berndtzen <93664614+GurraBern@users.noreply.github.com>
2024-11-20 21:40:27 +0100

    :wrench: add fix for calculating nutrition for different dates

---
+++ NutritionTrackR.Api/Features/NutrientTracking/SetNutritionGoal.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+
+namespace NutritionTrackR.Api.Features.NutrientTracking;
+
+public static class SetNutritionGoal 
+{
+    public static void MapSetNutritionGoal(this WebApplication app)
+    {
+        app.MapPost("api/v1/nutrition-goal/set", async (DateOnly date, IMediator mediator) =>
+        {
+            
+
+            return;
+        });
+    }
+}
---
+++ NutritionTrackR.Core/NutrientTracking/NutritionGoal.cs
@@ -0,0 +1,10 @@
+﻿using NutritionTrackR.Core.Foods.ValueObjects;
+
+namespace NutritionTrackR.Core.NutrientTracking;
+
+public class NutritionGoal
+{
+	public DateTime ActivationDate { get; set; }
+	
+	public List<Nutrient> Nutrients { get; set; } = [];
+}
