fb741e1b51ef55025dfba89cd05c183203f1eb74
Gustav Berndtzen <93664614+GurraBern@users.noreply.github.com>
2025-01-09 21:33:41 +0100

    refactor nutrients model to be more encapsulated

--- NutritionTrackR.Web/Pages/NutritionTarget/NutrientsModel.cs
+++ NutritionTrackR.Web/Pages/NutritionTarget/NutrientsModel.cs
@@ -1,4 +1,5 @@
 ﻿using NutritionTrackR.Contracts.Food;
+using NutritionTrackR.Web.Shared;
 using NutritionTrackR.Web.Shared.FoodSearch.AddFood;
 
 namespace NutritionTrackR.Web.Pages.NutritionTarget;
@@ -10,104 +11,115 @@ public class NutrientsModel
 		var nutrientDictionary = foods
 			.SelectMany(food => food.Nutrients)
 			.GroupBy(nutrient => nutrient.Name)
-			.ToDictionary(group => group.Key, group => new NutrientDto
+			.ToDictionary(group => group.Key, group => new NutrientWrapper(new NutrientDto
 			{
 				Name = group.Key,
 				Weight = group.Sum(nutrient => nutrient.Weight)
-			});
+			}));
 
 		foreach (var nutrient in NutrientList)
 		{
+			nutrient.ClearWeight();
 			if (nutrientDictionary.TryGetValue(nutrient.Name, out var combinedNutrient))
 			{
-				nutrient.From(combinedNutrient);
+				nutrient.AddWeight(combinedNutrient);
+			}
+		}
+	}
+
+	public void SetNutrientTargets(Dictionary<string, NutrientDto> nutrientTargets)
+	{
+		foreach (var nutrient in NutrientList)
+		{
+			if (nutrientTargets.TryGetValue(nutrient.Name, out var nutrientTarget))
+			{
+				nutrient.SetNutrientTarget(nutrientTarget);
 			}
 		}
 	}
 	
-	
-	//TODO set weight 0 to all
-	public NutrientDto Calories = new() { Name = "Calories", Unit = UnitDto.Kcal, Weight = 0 };
-	public NutrientDto Protein = new() { Name = "Protein", Unit = UnitDto.Grams, Weight = 0 };
-	public NutrientDto Carbohydrates = new() { Name = "Carbohydrates", Unit = UnitDto.Grams, Weight = 0 };
-	public NutrientDto Fiber = new() { Name = "Fiber", Unit = UnitDto.Grams, Weight = 0 }; //TODO how much?
-	public NutrientDto Sugars = new() { Name = "Sugars", Unit = UnitDto.Grams, Weight = 0 }; //TODO how much?
-	public NutrientDto Starch = new() { Name = "Starch", Unit = UnitDto.Grams, Weight = 0 }; //TODO how much?
-	
-	public NutrientDto Fat = new() { Name = "Fat", Unit = UnitDto.Grams, Weight = 70 };
-	public NutrientDto SaturatedFat = new() { Name = "SaturatedFat", Unit = UnitDto.Grams, Weight = 0 }; // As low as possible
-	public NutrientDto TransFat = new() { Name = "TransFat", Unit = UnitDto.Grams, Weight = 0 }; // As low as possible
-	public NutrientDto MonounsaturatedFat = new() { Name = "MonounsaturatedFat", Unit = UnitDto.Grams, Weight = 0 };
-	public NutrientDto PolyunsaturatedFat = new() { Name = "PolyunsaturatedFat", Unit = UnitDto.Grams, Weight = 0 };
-	public NutrientDto Cholesterol = new() { Name = "Cholesterol", Unit = UnitDto.Grams, Weight = 0 }; // As low as possible
-	public NutrientDto Omega3 = new() { Name = "Omega 3", Unit = UnitDto.Grams, Weight = 1.6 };
-	public NutrientDto Omega6 = new() { Name = "Omega 6", Unit = UnitDto.Grams, Weight = 17 };
-	
+	public NutrientWrapper Calories = new(new NutrientDto { Name = "Calories", Unit = UnitDto.Kcal, Weight = 0 });
+	public NutrientWrapper Protein = new(new NutrientDto { Name = "Protein", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper Carbohydrates = new(new NutrientDto { Name = "Carbohydrates", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper Fiber = new(new NutrientDto { Name = "Fiber", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper Sugars = new(new NutrientDto { Name = "Sugars", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper Starch = new(new NutrientDto { Name = "Starch", Unit = UnitDto.Grams, Weight = 0 });
+
+	public NutrientWrapper Fat = new(new NutrientDto { Name = "Fat", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper SaturatedFat = new(new NutrientDto { Name = "SaturatedFat", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper TransFat = new(new NutrientDto { Name = "TransFat", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper MonounsaturatedFat = new(new NutrientDto { Name = "MonounsaturatedFat", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper PolyunsaturatedFat = new(new NutrientDto { Name = "PolyunsaturatedFat", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper Cholesterol = new(new NutrientDto { Name = "Cholesterol", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper Omega3 = new(new NutrientDto { Name = "Omega 3", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper Omega6 = new(new NutrientDto { Name = "Omega 6", Unit = UnitDto.Grams, Weight = 0 });
+
 	// Vitamins
-	public NutrientDto VitaminA = new() { Name = "Vitamin A", Unit = UnitDto.Microgram, Weight = 900 }; 
-	public NutrientDto VitaminC = new() { Name = "Vitamin C", Unit = UnitDto.Milligram, Weight = 90 }; 
-	public NutrientDto VitaminD = new() { Name = "Vitamin D", Unit = UnitDto.Microgram, Weight = 15 }; 
-	public NutrientDto VitaminB6 = new() { Name = "Vitamin B6", Unit = UnitDto.Milligram, Weight = 1.3 }; 
-	public NutrientDto VitaminE = new() { Name = "Vitamin E", Unit = UnitDto.Milligram, Weight = 15 }; 
-	public NutrientDto VitaminK1 = new() { Name = "Vitamin K1", Unit = UnitDto.Microgram, Weight = 120 }; //TODO should be combined somehow with K2
-	public NutrientDto VitaminK2 = new() { Name = "Vitamin K2", Unit = UnitDto.Grams, Weight = 120 }; //TODO should be combined somehow with K1
-	public NutrientDto Thiamin = new() { Name = "Thiamin", Unit = UnitDto.Milligram, Weight = 1.2 }; 
-	public NutrientDto Riboflavin = new() { Name = "Riboflavin", Unit = UnitDto.Milligram, Weight = 1.3 }; 
-	public NutrientDto Niacin = new() { Name = "Niacin", Unit = UnitDto.Milligram, Weight = 16 }; 
-	public NutrientDto PantothenicAcid = new() { Name = "Pantothenic Acid", Unit = UnitDto.Milligram, Weight = 5 }; 
-	public NutrientDto Biotin = new() { Name = "Biotin", Unit = UnitDto.Microgram, Weight = 30 }; 
-	public NutrientDto Folate = new() { Name = "Folate", Unit = UnitDto.Microgram, Weight = 400 }; 
-	public NutrientDto VitaminB12 = new() { Name = "Vitamin B12", Unit = UnitDto.Microgram, Weight = 2.4 }; 
-	public NutrientDto TocopherolAlpha = new() { Name = "Tocopherol Alpha", Unit = UnitDto.Grams, Weight = 0 }; 
-	public NutrientDto Choline = new() { Name = "Choline", Unit = UnitDto.Grams, Weight = 0.55 }; 
-	public NutrientDto FolicAcid = new() { Name = "Folic Acid", Unit = UnitDto.Grams, Weight = 0 }; 
-	public NutrientDto CaroteneAlpha = new() { Name = "Carotene Alpha", Unit = UnitDto.Grams, Weight = 0 }; //?
-	public NutrientDto CaroteneBeta = new() { Name = "Carotene Beta", Unit = UnitDto.Grams, Weight = 0 }; //?
-	public NutrientDto CryptoxanthinBeta = new() { Name = "Cryptoxanthin Beta", Unit = UnitDto.Grams, Weight = 0 }; 
-	public NutrientDto LuteinZeaxanthin = new() { Name = "Lutein Zeaxanthin", Unit = UnitDto.Grams, Weight = 0 }; 
-	public NutrientDto Lycopene = new() { Name = "Lycopene", Unit = UnitDto.Grams, Weight = 0 };
-	
+	public NutrientWrapper VitaminA = new(new NutrientDto { Name = "Vitamin A", Unit = UnitDto.Microgram, Weight = 0 });
+	public NutrientWrapper VitaminC = new(new NutrientDto { Name = "Vitamin C", Unit = UnitDto.Milligram, Weight = 0 });
+	public NutrientWrapper VitaminD = new(new NutrientDto { Name = "Vitamin D", Unit = UnitDto.Microgram, Weight = 0 });
+	public NutrientWrapper VitaminB6 = new(new NutrientDto { Name = "Vitamin B6", Unit = UnitDto.Milligram, Weight = 0 });
+	public NutrientWrapper VitaminE = new(new NutrientDto { Name = "Vitamin E", Unit = UnitDto.Milligram, Weight = 0 });
+	public NutrientWrapper VitaminK1 = new(new NutrientDto { Name = "Vitamin K1", Unit = UnitDto.Microgram, Weight = 0 });
+	public NutrientWrapper VitaminK2 = new(new NutrientDto { Name = "Vitamin K2", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper Thiamin = new(new NutrientDto { Name = "Thiamin", Unit = UnitDto.Milligram, Weight = 0 });
+	public NutrientWrapper Riboflavin = new(new NutrientDto { Name = "Riboflavin", Unit = UnitDto.Milligram, Weight = 0 });
+	public NutrientWrapper Niacin = new(new NutrientDto { Name = "Niacin", Unit = UnitDto.Milligram, Weight = 0 });
+	public NutrientWrapper PantothenicAcid = new(new NutrientDto { Name = "Pantothenic Acid", Unit = UnitDto.Milligram, Weight = 0 });
+	public NutrientWrapper Biotin = new(new NutrientDto { Name = "Biotin", Unit = UnitDto.Microgram, Weight = 0 });
+	public NutrientWrapper Folate = new(new NutrientDto { Name = "Folate", Unit = UnitDto.Microgram, Weight = 0 });
+	public NutrientWrapper VitaminB12 = new(new NutrientDto { Name = "Vitamin B12", Unit = UnitDto.Microgram, Weight = 0 });
+	public NutrientWrapper TocopherolAlpha = new(new NutrientDto { Name = "Tocopherol Alpha", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper Choline = new(new NutrientDto { Name = "Choline", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper FolicAcid = new(new NutrientDto { Name = "Folic Acid", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper CaroteneAlpha = new(new NutrientDto { Name = "Carotene Alpha", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper CaroteneBeta = new(new NutrientDto { Name = "Carotene Beta", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper CryptoxanthinBeta = new(new NutrientDto { Name = "Cryptoxanthin Beta", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper LuteinZeaxanthin = new(new NutrientDto { Name = "Lutein Zeaxanthin", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper Lycopene = new(new NutrientDto { Name = "Lycopene", Unit = UnitDto.Grams, Weight = 0 });
+
 	// Minerals
-	public NutrientDto Calcium = new() { Name = "Calcium", Unit = UnitDto.Milligram, Weight = 1000 }; 
-	public NutrientDto Chloride = new() { Name = "Chloride", Unit = UnitDto.Grams, Weight = 2.3 }; 
-	public NutrientDto Chromium = new() { Name = "Chromium", Unit = UnitDto.Microgram, Weight = 35 }; 
-	public NutrientDto Iron = new() { Name = "Iron", Unit = UnitDto.Milligram, Weight = 8 };
-	public NutrientDto Zinc = new() { Name = "Zinc", Unit = UnitDto.Milligram, Weight = 11 };
-	public NutrientDto Sodium = new() { Name = "Sodium", Unit = UnitDto.Milligram, Weight = 1500 };
-	public NutrientDto Magnesium = new() { Name = "Magnesium", Unit = UnitDto.Milligram, Weight = 400 };
-	public NutrientDto Copper = new() { Name = "Copper", Unit = UnitDto.Microgram, Weight = 900 };
-	public NutrientDto Fluoride = new() { Name = "Fluoride", Unit = UnitDto.Milligram, Weight = 4 };
-	public NutrientDto Iodine = new() { Name = "Iodine", Unit = UnitDto.Microgram, Weight = 150 };
-	public NutrientDto Molybdenum = new() { Name = "Molybdenum", Unit = UnitDto.Microgram, Weight = 45 };
-	public NutrientDto Nickel = new() { Name = "Nickel", Unit = UnitDto.Milligram, Weight = 1 };
-	public NutrientDto Phosphorus = new() { Name = "Phosphorus", Unit = UnitDto.Grams, Weight = 0.7 };
-	public NutrientDto Potassium = new() { Name = "Potassium", Unit = UnitDto.Milligram, Weight = 3400 };
-	public NutrientDto Manganese = new() { Name = "Manganese", Unit = UnitDto.Milligram, Weight = 2.3 };
-	public NutrientDto Selenium = new() { Name = "Selenium", Unit = UnitDto.Microgram, Weight = 55 };
-	
+	public NutrientWrapper Calcium = new(new NutrientDto { Name = "Calcium", Unit = UnitDto.Milligram, Weight = 0 });
+	public NutrientWrapper Chloride = new(new NutrientDto { Name = "Chloride", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper Chromium = new(new NutrientDto { Name = "Chromium", Unit = UnitDto.Microgram, Weight = 0 });
+	public NutrientWrapper Iron = new(new NutrientDto { Name = "Iron", Unit = UnitDto.Milligram, Weight = 0 });
+	public NutrientWrapper Zinc = new(new NutrientDto { Name = "Zinc", Unit = UnitDto.Milligram, Weight = 0 });
+	public NutrientWrapper Sodium = new(new NutrientDto { Name = "Sodium", Unit = UnitDto.Milligram, Weight = 0 });
+	public NutrientWrapper Magnesium = new(new NutrientDto { Name = "Magnesium", Unit = UnitDto.Milligram, Weight = 0 });
+	public NutrientWrapper Copper = new(new NutrientDto { Name = "Copper", Unit = UnitDto.Microgram, Weight = 0 });
+	public NutrientWrapper Fluoride = new(new NutrientDto { Name = "Fluoride", Unit = UnitDto.Milligram, Weight = 0 });
+	public NutrientWrapper Iodine = new(new NutrientDto { Name = "Iodine", Unit = UnitDto.Microgram, Weight = 0 });
+	public NutrientWrapper Molybdenum = new(new NutrientDto { Name = "Molybdenum", Unit = UnitDto.Microgram, Weight = 0 });
+	public NutrientWrapper Nickel = new(new NutrientDto { Name = "Nickel", Unit = UnitDto.Milligram, Weight = 0 });
+	public NutrientWrapper Phosphorus = new(new NutrientDto { Name = "Phosphorus", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper Potassium = new(new NutrientDto { Name = "Potassium", Unit = UnitDto.Milligram, Weight = 0 });
+	public NutrientWrapper Manganese = new(new NutrientDto { Name = "Manganese", Unit = UnitDto.Milligram, Weight = 0 });
+	public NutrientWrapper Selenium = new(new NutrientDto { Name = "Selenium", Unit = UnitDto.Microgram, Weight = 0 });
+
 	// Amino Acids
-	public NutrientDto Alanine = new() { Name = "Alanine", Unit = UnitDto.Grams, Weight = 0 };
-	public NutrientDto Arginine = new() { Name = "Arginine", Unit = UnitDto.Grams, Weight = 0 }; // Nonessential
-	public NutrientDto Asparagine = new() { Name = "Asparagine", Unit = UnitDto.Grams, Weight = 0 };
-	public NutrientDto AsparticAcid = new() { Name = "Aspartic Acid", Unit = UnitDto.Grams, Weight = 0 };
-	public NutrientDto Cystine = new() { Name = "Cystine", Unit = UnitDto.Grams, Weight = 0 }; // Nonessential
-	public NutrientDto GlutamicAcid = new() { Name = "Glutamic Acid", Unit = UnitDto.Grams, Weight = 0 };
-	public NutrientDto Glutamine = new() { Name = "Glutamine", Unit = UnitDto.Grams, Weight = 0 }; // Nonessential
-	public NutrientDto Glycine = new() { Name = "Glycine", Unit = UnitDto.Grams, Weight = 0 }; // Nonessential
-	public NutrientDto Histidine = new() { Name = "Histidine", Unit = UnitDto.Milligram, Weight = 952 };
-	public NutrientDto Hydroxyproline = new() { Name = "Hydroxyproline", Unit = UnitDto.Grams, Weight = 0 };
-	public NutrientDto Isoleucine = new() { Name = "Isoleucine", Unit = UnitDto.Milligram, Weight = 1292 };
-	public NutrientDto Leucine = new() { Name = "Leucine", Unit = UnitDto.Milligram, Weight = 2856 };
-	public NutrientDto Lysine = new() { Name = "Lysine", Unit = UnitDto.Milligram, Weight = 2584 };
-	public NutrientDto Methionine = new() { Name = "Methionine", Unit = UnitDto.Milligram, Weight = 1292 };
-	public NutrientDto Phenylalanine = new() { Name = "Phenylalanine", Unit = UnitDto.Milligram, Weight = 2244 };
-	public NutrientDto Proline = new() { Name = "Proline", Unit = UnitDto.Grams, Weight = 0 }; // Nonessential
-	public NutrientDto Serine = new() { Name = "Serine", Unit = UnitDto.Grams, Weight = 0 }; // Nonessential
-	public NutrientDto Threonine = new() { Name = "Threonine", Unit = UnitDto.Milligram, Weight = 1360 };
-	public NutrientDto Tryptophan = new() { Name = "Tryptophan", Unit = UnitDto.Milligram, Weight = 340 };
-	public NutrientDto Tyrosine = new() { Name = "Tyrosine", Unit = UnitDto.Grams, Weight = 0 }; // Nonessential
-	public NutrientDto Valine = new() { Name = "Valine", Unit = UnitDto.Milligram, Weight = 1632 };
+	public NutrientWrapper Alanine = new(new NutrientDto { Name = "Alanine", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper Arginine = new(new NutrientDto { Name = "Arginine", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper Asparagine = new(new NutrientDto { Name = "Asparagine", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper AsparticAcid = new(new NutrientDto { Name = "Aspartic Acid", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper Cystine = new(new NutrientDto { Name = "Cystine", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper GlutamicAcid = new(new NutrientDto { Name = "Glutamic Acid", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper Glutamine = new(new NutrientDto { Name = "Glutamine", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper Glycine = new(new NutrientDto { Name = "Glycine", Unit = UnitDto.Grams, Weight = 0 });
+	public NutrientWrapper Histidine = new(new NutrientDto { Name = "Histidine", Unit = UnitDto.Milligram, Weight = 0 });
+	public NutrientWrapper Hydroxyproline = new(new NutrientDto { Name = "Hydroxyproline", Unit = UnitDto.Grams, Weight = 0 });
+    public NutrientWrapper Isoleucine = new(new NutrientDto { Name = "Isoleucine", Unit = UnitDto.Milligram, Weight = 0 });
+    public NutrientWrapper Leucine = new(new NutrientDto { Name = "Leucine", Unit = UnitDto.Milligram, Weight = 0 });
+	public NutrientWrapper Lysine = new(new NutrientDto { Name = "Lysine", Unit = UnitDto.Milligram, Weight = 0 });
+    public NutrientWrapper Methionine = new(new NutrientDto { Name = "Methionine", Unit = UnitDto.Milligram, Weight = 0 });
+    public NutrientWrapper Phenylalanine = new(new NutrientDto { Name = "Phenylalanine", Unit = UnitDto.Milligram, Weight = 0 });
+    public NutrientWrapper Proline = new(new NutrientDto { Name = "Proline", Unit = UnitDto.Grams, Weight = 0 }); // Nonessential
+    public NutrientWrapper Serine = new(new NutrientDto { Name = "Serine", Unit = UnitDto.Grams, Weight = 0 }); // Nonessential
+    public NutrientWrapper Threonine = new(new NutrientDto { Name = "Threonine", Unit = UnitDto.Milligram, Weight = 0 });
+    public NutrientWrapper Tryptophan = new(new NutrientDto { Name = "Tryptophan", Unit = UnitDto.Milligram, Weight = 0 });
+    public NutrientWrapper Tyrosine = new(new NutrientDto { Name = "Tyrosine", Unit = UnitDto.Grams, Weight = 0 }); // Nonessential
+    public NutrientWrapper Valine = new(new NutrientDto { Name = "Valine", Unit = UnitDto.Milligram, Weight = 0 });
 	// public static NutrientDto Theobromine => new() { Name = "Theobromine", Unit = UnitDto.Grams, Weight = 0 };
 
-	public List<NutrientDto> NutrientList => [Calories, Protein, Carbohydrates, Fiber, Sugars, Starch, Fat, SaturatedFat, TransFat, MonounsaturatedFat, PolyunsaturatedFat, Chloride, Omega3, Omega6, Cholesterol, VitaminA, VitaminD, VitaminE, VitaminC, VitaminK1, VitaminK2, Thiamin, Riboflavin, Niacin, PantothenicAcid, VitaminB6, Biotin, Folate, VitaminB12, TocopherolAlpha, Choline, FolicAcid, CaroteneAlpha, CaroteneBeta, CryptoxanthinBeta, LuteinZeaxanthin, Lycopene, Calcium, Chromium, Iron, Zinc, Sodium, Magnesium, Copper, Fluoride, Iodine, Molybdenum, Nickel, Phosphorus, Potassium, Manganese, Phosphorus, Selenium, Alanine, Arginine, Asparagine, AsparticAcid, Cystine, GlutamicAcid, Glutamine, Glycine, Histidine, Hydroxyproline, Isoleucine, Leucine, Lysine, Methionine, Phenylalanine, Proline, Serine, Threonine, Tryptophan, Tyrosine, Valine];
+	public List<NutrientWrapper> NutrientList => [Calories, Protein, Carbohydrates, Fiber, Sugars, Starch, Fat, SaturatedFat, TransFat, MonounsaturatedFat, PolyunsaturatedFat, Chloride, Omega3, Omega6, Cholesterol, VitaminA, VitaminD, VitaminE, VitaminC, VitaminK1, VitaminK2, Thiamin, Riboflavin, Niacin, PantothenicAcid, VitaminB6, Biotin, Folate, VitaminB12, TocopherolAlpha, Choline, FolicAcid, CaroteneAlpha, CaroteneBeta, CryptoxanthinBeta, LuteinZeaxanthin, Lycopene, Calcium, Chromium, Iron, Zinc, Sodium, Magnesium, Copper, Fluoride, Iodine, Molybdenum, Nickel, Phosphorus, Potassium, Manganese, Phosphorus, Selenium, Alanine, Arginine, Asparagine, AsparticAcid, Cystine, GlutamicAcid, Glutamine, Glycine, Histidine, Hydroxyproline, Isoleucine, Leucine, Lysine, Methionine, Phenylalanine, Proline, Serine, Threonine, Tryptophan, Tyrosine, Valine];
 }
+
---
+++ NutritionTrackR.Web/Shared/NutrientWrapper.cs
@@ -0,0 +1,49 @@
+﻿using NutritionTrackR.Contracts.Food;
+
+namespace NutritionTrackR.Web.Shared;
+
+public class NutrientWrapper(NutrientDto trackedNutrient)
+{
+    private const double MaxProgress = 100;
+    private NutrientDto TrackedNutrient { get; } = trackedNutrient;
+
+    //TODO what happens if new() maybe rethink?
+    private NutrientDto NutritionTarget { get; set; } = new();
+
+    public string Name => TrackedNutrient.Name;
+    public UnitDto Unit => TrackedNutrient.Unit;
+    public double Weight => TrackedNutrient.Weight;
+    public bool IsComplete => GetProgress() > MaxProgress;
+
+    public void AddWeight(NutrientWrapper nutrient)
+    {
+        TrackedNutrient.Weight += nutrient.Weight; //TODO need to handle addition with the use of Unit
+    }
+
+    public void SetNutrientTarget(NutrientDto nutrientTarget)
+    {
+        NutritionTarget = nutrientTarget;
+    }
+    public double GetProgress()
+    {
+        if (NutritionTarget.Weight == 0)
+            return MaxProgress;
+    
+        return Math.Round(TrackedNutrient.Weight/NutritionTarget.Weight, 2) * MaxProgress;
+    }
+
+    public void ClearWeight()
+    {
+        TrackedNutrient.Weight = 0;
+    }
+
+    public string ToProgressDisplayString() => Math.Round(TrackedNutrient.Weight, 2) + "/" + NutritionTarget.Weight + " " + NutritionTarget.Unit;
+
+    private static double ConvertUnit(NutrientDto nutrient) => nutrient.Unit switch
+    {
+        UnitDto.Grams => nutrient.Weight,
+        UnitDto.Milligram => nutrient.Weight * 1_000, 
+        UnitDto.Microgram => nutrient.Weight * 1000_000,
+        _ => -1
+    };
+}
